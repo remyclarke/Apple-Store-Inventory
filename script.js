41a47f94b6c43b2abb90edb38dec9bf7458bda9e
Sincere <sincereclarke@pursuit.org>
2023-11-03 17:06:44 -0400

    Bug fixes, Added more styles, Completed Project

--- script.js
+++ script.js
@@ -1,38 +1,17 @@
-// Function to update local storage with the current product list
-function updateLocalStorage() {
-    const productCards = document.querySelectorAll('.product-card');
-    const products = [];
-
-    productCards.forEach(card => {
-        const name = card.querySelector('h3').textContent;
-        const price = card.querySelector('.price').textContent.replace('$', '');
-        const inStock = card.querySelector('.stock-status').classList.contains('in-stock');
-        const imageSrc = card.querySelector('img').getAttribute('src');
-
-        products.push({ name, price, inStock, imageSrc });
-    });
-
-    localStorage.setItem('products', JSON.stringify(products));
+// Function to reset the form
+function resetForm() {
+    document.getElementById('product-form').reset();
 }
 
-// Function to load products from local storage
-function loadProducts() {
-    const products = JSON.parse(localStorage.getItem('products')) || [];
-
-    products.forEach(product => {
-        createProductCard(product.name, product.price, product.inStock, product.imageSrc);
-    });
-}
-
-// Function to create a product card
-function createProductCard(name, price, inStock, imageSrc = 'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg') {
+// Function to add a product card to the page
+function addProductCard(name, price, inStock, imageSrc) {
     const productCard = document.createElement('div');
     productCard.className = 'product-card';
     productCard.innerHTML = `
         <img src="${imageSrc}" alt="${name}">
         <h3>${name}</h3>
         <p class="price">$${parseFloat(price).toFixed(2)}</p>
-        <p class="stock-status ${inStock ? 'in-stock' : 'out-of-stock'}">
+        <p class="${inStock ? 'in-stock' : 'out-of-stock'}">
             ${inStock ? 'In Stock' : 'Out of Stock'}
         </p>
         <button onclick="removeProduct(this)">Remove</button>
@@ -42,7 +21,7 @@ function createProductCard(name, price, inStock, imageSrc = 'https://static.vect
     updateLocalStorage();
 }
 
-// Function to remove a product
+// Function to remove a product card and update localStorage
 function removeProduct(button) {
     button.parentNode.remove();
     updateLocalStorage();
@@ -54,12 +33,38 @@ document.getElementById('product-form').addEventListener('submit', function(even
     
     const name = document.getElementById('name').value;
     const price = document.getElementById('price').value;
-    const inStock = document.getElementById('inStock').checked;
-    const image = document.getElementById('image').value || 'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg';
+    const inStock = document.getElementById('inStock').value === 'true';
+    const imageSrc = document.getElementById('image').value || 'https://static.vecteezy.com/system/resources/previews/005/337/799/original/icon-image-not-found-free-vector.jpg';
     
-    createProductCard(name, price, inStock, image);
-    document.getElementById('product-form').reset();
+    addProductCard(name, price, inStock, imageSrc);
+    resetForm();
 });
 
-// Load products from local storage when the page loads
-document.addEventListener('DOMContentLoaded', loadProducts);
+// Function to update the inventory in localStorage
+function updateLocalStorage() {
+    const productCards = document.querySelectorAll('.product-card');
+    const inventory = [];
+
+    productCards.forEach(card => {
+        const name = card.querySelector('h3').innerText;
+        const price = card.querySelector('.price').innerText.replace('$', '');
+        const inStock = card.querySelector('.in-stock') !== null;
+        const imageSrc = card.querySelector('img').src;
+
+        inventory.push({ name, price, inStock, imageSrc });
+    });
+
+    localStorage.setItem('inventory', JSON.stringify(inventory));
+}
+
+// Function to load the inventory from localStorage
+function loadInventory() {
+    const inventory = JSON.parse(localStorage.getItem('inventory')) || [];
+
+    inventory.forEach(item => {
+        addProductCard(item.name, item.price, item.inStock, item.imageSrc);
+    });
+}
+
+// Load the inventory when the page is loaded
+document.addEventListener('DOMContentLoaded', loadInventory);
